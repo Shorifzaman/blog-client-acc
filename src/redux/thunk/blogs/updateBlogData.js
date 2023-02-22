a2439f5bb430faca50e3c173f1bf78b0925fa726
shorifzaman <shorif.zaman@gmail.com>
2023-02-23 03:10:21 +0600

    [ADD] added API development

--- src/redux/thunk/blogs/updateBlogData.js
+++ src/redux/thunk/blogs/updateBlogData.js
@@ -2,8 +2,7 @@ import { updateBlog } from "../../actions/blogsActions";
 
 const updateBlogData = (blog, navigate) => {
     return async (dispatch, getState) => {
-        const res = await fetch('http://localhost:5000/blog', {
-        // const res = await fetch('https://ocean-of-blogger-api.onrender.com/blog', {
+        const res = await fetch('https://blog-server-shorifzaman.vercel.app//blog', {
             method: 'PATCH',
             headers: {
                 "content-type": "application/json",
